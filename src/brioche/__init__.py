589f069a2519d24896200ec37c946e226720ac2e
Peter Liljenberg <peter.liljenberg@gmail.com>
2021-04-14 19:10:05 +0200

    Bump version to 0.3.0

--- src/brioche/__init__.py
+++ src/brioche/__init__.py
@@ -4,7 +4,7 @@
 """Python/Pandas library to do biomization analyses of pollen samples
 """
 
-__version__ = "0.2.1"
+__version__ = "0.3.0"
 
 from .helpers import safe_int, dataframe_from_gspread_sheet
 
